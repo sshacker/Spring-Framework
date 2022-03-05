bbb51014a8b6f240a57b996a11b969094663782a
sshacker <shivamsonkar260@gmail.com>
2022-03-05 17:22:33 +0530

    Injecting Collection Types[ List , Set , Map , Properties ] | Practical | Spring Framework Tutorial

---
+++ springcore/src/main/java/com/sshacker/durgesh/springcore/collection/TestMain.java
@@ -0,0 +1,17 @@
+package com.sshacker.durgesh.springcore.collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/sshacker/durgesh/springcore/collection/collectionconfig.xml");
+		Emp e1 = (Emp) context.getBean("emp1");
+		System.out.println(e1.getName());
+		System.out.println(e1.getPhones());
+		System.out.println(e1.getAddresses());
+		System.out.println(e1.getCourses());
+	}
+
+}
