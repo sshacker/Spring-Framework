01ba54a5babdd7d7fe30c4043f1249b7479e317f
sshacker <shivamsonkar260@gmail.com>
2022-03-05 14:52:18 +0530

    adding spring-core and spring-context in pom.xml, createing Student class bean, config.xml adding student1 bean, getting object by ApplicationContext from IoC container

--- springcore/src/main/java/com/sshacker/durgesh/springcore/App.java
+++ springcore/src/main/java/com/sshacker/durgesh/springcore/App.java
@@ -1,13 +1,17 @@
 package com.sshacker.durgesh.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		Student s1 = (Student) context.getBean("student1");
+		System.out.println(s1);
+ 	}
 }
