f52861febe39cfa7063e0d751126b2f43a8c3037
sshacker <shivamsonkar260@gmail.com>
2022-03-10 07:14:22 +0530

    Stereotype Annotations | @Component Annotation | @Value Annotation - @Component

---
+++ springcore/src/main/java/com/sshacker/springcore/stereotype/MainApp.java
@@ -0,0 +1,14 @@
+package com.sshacker.springcore.stereotype;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/sshacker/springcore/stereotype/stereotyepconfig.xml");
+		Student s1 = (Student) context.getBean("student", Student.class);
+		System.out.println(s1);
+	}
+}
